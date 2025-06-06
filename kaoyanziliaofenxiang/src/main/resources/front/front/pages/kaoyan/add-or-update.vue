4546ed87857021d450bbd99741561e8a65eb2948
domie163 <domie1948@163.com>
2025-06-06 20:50:36 +0800

    Initial commit

---
+++ kaoyanziliaofenxiang/src/main/resources/front/front/pages/kaoyan/add-or-update.vue
@@ -0,0 +1,341 @@
+
+<template>
+	<view class="content">
+		<form class="app-update-pv">
+            <view
+                    :style='{"boxShadow":"0","backgroundColor":"rgba(255, 255, 255, 0.25)","borderColor":"rgba(228, 232, 230, 1)","margin":"0 0 0px 0","borderWidth":"2rpx","borderStyle":"none none solid none ","height":"88rpx"}'
+                    class="cu-form-group">
+                <view :style='{"width":"160rpx","fontSize":"28rpx","color":"#f06f4f","textAlign":"left"}'
+                      class="title">考研信息标题</view>
+                <input   disabled
+                         :style='{"boxShadow":"0 0 0px rgba(0,0,0,.6) inset","backgroundColor":"rgba(255, 255, 255, 1)","borderColor":"#f06f4f","borderRadius":"0px","color":"#f06f4f","textAlign":"left","borderWidth":"0 0 6rpx","fontSize":"24rpx","borderStyle":"solid","height":"88rpx","marginTop":"7rpx"}'
+                         :disabled="ro.kaoyanName" type="text" v-model="ruleForm.kaoyanName" placeholder="考研信息标题"></input>
+            </view>
+                <view :style='{"boxShadow":"0","backgroundColor":"rgba(255, 255, 255, 0.25)","borderColor":"rgba(228, 232, 230, 1)","margin":"0 0 0px 0","borderWidth":"2rpx","borderStyle":"none none solid none ","height":"88rpx"}'
+                      class="cu-form-group">
+                    <view :style='{"width":"160rpx","fontSize":"28rpx","color":"#f06f4f","textAlign":"left"}'
+                          class="title">考研信息类型</view>
+                    <picker @change="kaoyanTypesChange" :value="kaoyanTypesIndex" :range="kaoyanTypesOptions" range-key="indexName">
+                        <view
+                                :style='{"boxShadow":"0 0 0px rgba(0,0,0,.6) inset","backgroundColor":"rgba(255, 255, 255, 1)","borderColor":"#f06f4f","borderRadius":"0px","color":"#f06f4f","textAlign":"left","borderWidth":"0 0 6rpx","fontSize":"24rpx","borderStyle":"solid","height":"88rpx","marginTop":"7rpx"}'
+                                class="uni-input">{{ruleForm.kaoyanTypes?ruleForm.kaoyanValue:"请选择考研信息类型"}}</view>
+                    </picker>
+                </view>
+                <view
+                        :style='{"boxShadow":"0 0 0px rgba(0,0,0,.3)","backgroundColor":"rgba(255, 255, 255, 0.25)","borderColor":"rgba(218, 220, 219, 1)","margin":"0 0 0px 0","borderWidth":"2rpx","borderStyle":"none none solid none ","height":"108rpx"}'
+                        class="cu-form-group" @tap="kaoyanPhotoTap">
+                    <view :style='{"width":"160rpx","fontSize":"28rpx","color":"#f06f4f","textAlign":"left"}'
+                          class="title">考研信息封面</view>
+                    <view class="right-input" style="padding:0;textAlign:left">
+                        <image
+                                :style='{"width":"68rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","borderRadius":"100%","textAlign":"left","height":"68rpx"}'
+                                class="avator" v-if="ruleForm.kaoyanPhoto" :src="ruleForm.kaoyanPhoto" mode="aspectFill">
+                        </image>
+                        <image  :style='{"width":"68rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","borderRadius":"100%","textAlign":"left","height":"68rpx"}'
+                                class="avator" v-else src="../../static/gen/upload.png" mode="aspectFill"></image>
+                    </view>
+                </view>
+			                <view
+                        :style='{"boxShadow":"0 0 0px rgba(0,0,0,.3)","backgroundColor":"rgba(255, 255, 255, 0.25)","borderColor":"rgba(218, 220, 219, 1)","margin":"0 0 0px 0","borderWidth":"2rpx","borderStyle":"none none solid none ","height":"108rpx"}'
+                        class="cu-form-group" @tap="kaoyanFileTap">
+                    <view :style='{"width":"160rpx","fontSize":"28rpx","color":"#f06f4f","textAlign":"left"}'
+                          class="title">考研资料</view>
+                    <view class="right-input" style="padding:0;textAlign:left">
+                        <image
+                                :style='{"width":"68rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","borderRadius":"100%","textAlign":"left","height":"68rpx"}'
+                                class="avator" v-if="ruleForm.kaoyanFile" :src="ruleForm.kaoyanFile" mode="aspectFill">
+                        </image>
+                        <image  :style='{"width":"68rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","borderRadius":"100%","textAlign":"left","height":"68rpx"}'
+                                class="avator" v-else src="../../static/gen/upload.png" mode="aspectFill"></image>
+                    </view>
+                </view>
+			                <view
+                        :style='{"boxShadow":"0 0 0px rgba(0,0,0,.3)","backgroundColor":"rgba(255, 255, 255, 0.25)","borderColor":"rgba(218, 220, 219, 1)","margin":"0 0 0px 0","borderWidth":"2rpx","borderStyle":"none none solid none ","height":"108rpx"}'
+                        class="cu-form-group" @tap="kaoyanVideoTap">
+                    <view :style='{"width":"160rpx","fontSize":"28rpx","color":"#f06f4f","textAlign":"left"}'
+                          class="title">考研视频</view>
+                    <view class="right-input" style="padding:0;textAlign:left">
+                        <image
+                                :style='{"width":"68rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","borderRadius":"100%","textAlign":"left","height":"68rpx"}'
+                                class="avator" v-if="ruleForm.kaoyanVideo" :src="ruleForm.kaoyanVideo" mode="aspectFill">
+                        </image>
+                        <image  :style='{"width":"68rpx","boxShadow":"0 0 0px rgba(0,0,0,.3)","borderRadius":"100%","textAlign":"left","height":"68rpx"}'
+                                class="avator" v-else src="../../static/gen/upload.png" mode="aspectFill"></image>
+                    </view>
+                </view>
+			<!-- text后缀 -->
+            <view
+                    :style='{"boxShadow":"0","backgroundColor":"rgba(255, 255, 255, 0.25)","borderColor":"rgba(228, 232, 230, 1)","margin":"0 0 0px 0","borderWidth":"2rpx","borderStyle":"none none solid none ","height":"210rpx"}'
+                    class="cu-form-group">
+                <view :style='{"width":"160rpx","fontSize":"28rpx","color":"#f06f4f","textAlign":"left"}'
+                      class="title">考研详情</view>
+                <textarea :style='{"boxShadow":"0 0 0px rgba(0,0,0,.6) inset","backgroundColor":"rgba(255, 255, 255, 1)","borderColor":"#f06f4f","borderRadius":"0px","color":"#f06f4f","textAlign":"left","borderWidth":"0 0 6rpx","fontSize":"24rpx","borderStyle":"solid","height":"88rpx","marginTop":"7rpx","height":"200rpx"}'
+                          :disabled="ro.kaoyanContent" v-model="ruleForm.kaoyanContent" placeholder="考研详情"/>
+            </view>
+
+			<view class="btn">
+				<button
+					:style='{"boxShadow":"0 0 16rpx rgba(0,0,0,0) inset","backgroundColor":"#f06f4f","borderColor":"#409EFF","borderRadius":"8rpx","color":"rgba(255, 255, 255, 1)","borderWidth":"0","width":"70%","fontSize":"32rpx","borderStyle":"solid","height":"80rpx"}'
+					@tap="onSubmitTap" class="bg-red">提交</button>
+			</view>
+		</form>
+
+					<w-picker mode="dateTime" step="1" :current="false" :hasSecond="false" @confirm="createTimeConfirm"
+						  ref="createTime" themeColor="#333333"></w-picker>
+
+
+	</view>
+</template>
+
+<script>
+	import wPicker from "@/components/w-picker/w-picker.vue";
+
+	export default {
+		data() {
+			return {
+				cross: '',
+                ro:{
+					kaoyanName: false,
+					kaoyanTypes: false,
+					kaoyanPhoto: false,
+					kaoyanFile: false,
+					kaoyanVideo: false,
+					yonghuId: false,
+					kaoyanContent: false,
+					createTime: false,
+				},
+				ruleForm: {
+					kaoyanName: '',
+					kaoyanTypes: '',//数字
+					kaoyanValue: '',//数字对应的值
+					kaoyanPhoto: '',
+					kaoyanFile: '',
+					kaoyanVideo: '',
+					yonghuId: '',
+					kaoyanContent: '',
+					createTime: '',
+				},
+				// 登陆用户信息
+				user: {},
+				kaoyanTypesOptions: [],
+				kaoyanTypesIndex : 0,
+
+			}
+		},
+		components: {
+			wPicker
+		},
+		computed: {
+			baseUrl() {
+				return this.$base.url;
+			},
+		},
+		async onLoad(options) {
+
+		/*下拉框*/
+			let kaoyanTypesParams = {
+				page: 1,
+				limit: 100,
+				dicCode: 'kaoyan_types',
+			}
+			let kaoyanTypes = await this.$api.page(`dictionary`, kaoyanTypesParams);
+			this.kaoyanTypesOptions = kaoyanTypes.data.list
+
+
+			// 如果是更新操作
+			if (options.id) {
+				this.ruleForm.id = options.id;
+				// 获取信息
+				let res = await this.$api.info(`kaoyan`, this.ruleForm.id);
+				this.ruleForm = res.data;
+			}
+			if(options.kaoyanId){
+                this.ruleForm.kaoyanId = options.kaoyanId;
+			}
+			// 跨表
+			// this.styleChange()
+		},
+		methods: {
+			// 下拉变化
+            kaoyanTypesChange(e) {
+            this.kaoyanTypesIndex = e.target.value
+            this.ruleForm.kaoyanValue = this.kaoyanTypesOptions[this.kaoyanTypesIndex].indexName
+            this.ruleForm.kaoyanTypes = this.kaoyanTypesOptions[this.kaoyanTypesIndex].codeIndex
+        },
+
+
+			// styleChange() {
+			// 	this.$nextTick(() => {
+			// 		// document.querySelectorAll('.app-update-pv .cu-form-group .uni-yaoxianStyle-yaoxianStyle').forEach(el=>{
+			// 		//   el.style.backgroundColor = this.addUpdateForm.yaoxianStyle.content.backgroundColor
+			// 		// })
+			// 	})
+			// },
+
+
+			kaoyanPhotoTap() {
+				let _this = this;
+				this.$api.upload(function(res) {
+					_this.ruleForm.kaoyanPhoto = _this.$base.url + 'upload/' + res.file;
+					_this.$forceUpdate();
+					_this.$nextTick(() => {
+						// _this.styleChange()
+					})
+				});
+			},
+			kaoyanFileTap() {
+				let _this = this;
+				this.$api.upload(function(res) {
+					_this.ruleForm.kaoyanFile = _this.$base.url + 'upload/' + res.file;
+					_this.$forceUpdate();
+					_this.$nextTick(() => {
+						// _this.styleChange()
+					})
+				});
+			},
+			kaoyanVideoTap() {
+				let _this = this;
+				this.$api.upload(function(res) {
+					_this.ruleForm.kaoyanVideo = _this.$base.url + 'upload/' + res.file;
+					_this.$forceUpdate();
+					_this.$nextTick(() => {
+						// _this.styleChange()
+					})
+				});
+			},
+			// 日期控件
+			createTimeConfirm(val) {
+                console.log(val)
+                this.ruleForm.createTime = val.result;
+                this.$forceUpdate();
+            },
+
+
+
+
+			getUUID() {
+				return new Date().getTime();
+			},
+			async onSubmitTap() {
+				if ((!this.ruleForm.kaoyanName)) {
+					this.$utils.msg(`考研信息标题不能为空`);
+					return
+				}
+				if ((!this.ruleForm.kaoyanPhoto)) {
+					this.$utils.msg(`考研信息封面不能为空`);
+					return
+				}
+				if (this.ruleForm.id) {
+					await this.$api.update(`kaoyan`, this.ruleForm);
+				} else {
+					await this.$api.save(`kaoyan`, this.ruleForm);
+				}
+                uni.setStorageSync('pingluenStateState', true);
+                this.$utils.msgBack('提交成功');
+			},
+			getDate(type) {
+				const date = new Date();
+				let year = date.getFullYear();
+				let month = date.getMonth() + 1;
+				let day = date.getDate();
+				if (type === 'start') {
+					year = year - 60;
+				} else if (type === 'end') {
+					year = year + 2;
+				}
+				month = month > 9 ? month : '0' + month;;
+				day = day > 9 ? day : '0' + day;
+				return `${year}-${month}-${day}`;
+			},
+			toggleTab(str) {
+				this.$refs[str].show();
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		padding: 20upx;
+	}
+
+	.content:after {
+		position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		bottom: 0;
+		content: '';
+		background-attachment: fixed;
+		background-size: cover;
+		background-position: center;
+	}
+
+	textarea {
+		border: 1upx solid #EEEEEE;
+		border-radius: 20upx;
+		padding: 20upx;
+	}
+
+	.title {
+		width: 180upx;
+	}
+
+	.avator {
+		width: 150upx;
+		height: 60upx;
+	}
+
+	.right-input {
+		flex: 1;
+		text-align: left;
+		padding: 0 24upx;
+	}
+
+	.cu-form-group.active {
+		justify-content: space-between;
+	}
+
+	.btn {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-wrap: wrap;
+		padding: 20upx 0;
+	}
+
+	.cu-form-group {
+		padding: 0 24upx;
+		background-color: transparent;
+		min-height: inherit;
+	}
+
+	.cu-form-group+.cu-form-group {
+		border: 0;
+	}
+
+	.cu-form-group uni-input {
+		padding: 0 30upx;
+	}
+
+	.uni-input {
+		padding: 0 30upx;
+	}
+
+	.cu-form-group uni-textarea {
+		padding: 30upx;
+		margin: 0;
+	}
+
+	.cu-form-group uni-picker::after {
+		line-height: 68rpx;
+	}
+
+	.select .uni-input {
+		line-height: 68rpx;
+	}
+
+	.input .right-input {
+		line-height: 68rpx;
+	}
+</style>
